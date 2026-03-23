66353aec791906edd3c916901d523731eb96ee9f
李昌言 <lcy.adsl.tl@gmail.com>
2026-03-23 06:06:23 +0000

    變更

---
+++ lesson8/lesson8_2.py
@@ -0,0 +1,35 @@
+import random
+
+play_count = 0
+while True:
+    play_count += 1
+    min = 1
+    max = 100
+    count = 0
+    guess_value = random.randint(min,max)
+    print("==========猜數字遊戲===========\n")
+    while(True):
+        keyin = int(input(f"猜數字範圍{min}~{max}\n"))
+        count += 1
+        if keyin >= min and keyin <= max:
+            if(keyin == guess_value):
+                print(f"賓果!猜對了,答案是:{guess_value}")
+                print(f"您猜了{count}次")
+                break
+            elif keyin > guess_value:
+                print("再小一點!")
+                max = keyin - 1
+            
+            else:
+                print("再大一點")
+                min = keyin + 1
+
+            print(f"您已經猜{count}次")
+            
+        else:
+            print("請輸入提示範圍內的數字")
+    is_continue = input("您還要繼續嗎(y,n)?")
+    if is_continue == "n":
+        break
+print(f"您共玩了{play_count}次")
+print("遊戲結束")
